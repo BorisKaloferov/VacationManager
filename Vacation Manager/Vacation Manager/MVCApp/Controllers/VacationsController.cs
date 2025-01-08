4f08d6c9c1efb937a1b2bf483cd19a81af8eb44a
Dimitar25 <dimitar.bazlyankov@gmail.com>
2025-01-08 16:31:06 +0200

    Controllers created

---
+++ Vacation Manager/Vacation Manager/MVCApp/Controllers/VacationsController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Business_Layer;
+using Data_Layer;
+
+namespace MVCApp.Controllers
+{
+    public class VacationsController : Controller
+    {
+        private readonly VacationManagerDbContext _context;
+
+        public VacationsController(VacationManagerDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Vacations
+        public async Task<IActionResult> Index()
+        {
+            var vacationManagerDbContext = _context.Vacations.Include(v => v.User);
+            return View(await vacationManagerDbContext.ToListAsync());
+        }
+
+        // GET: Vacations/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vacation = await _context.Vacations
+                .Include(v => v.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vacation == null)
+            {
+                return NotFound();
+            }
+
+            return View(vacation);
+        }
+
+        // GET: Vacations/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Vacations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,StartDate,EndDate,CreationDate,IsHalfDay,IsConfirmed,VacationType,SickSheet,UserId")] Vacation vacation)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(vacation);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", vacation.UserId);
+            return View(vacation);
+        }
+
+        // GET: Vacations/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vacation = await _context.Vacations.FindAsync(id);
+            if (vacation == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", vacation.UserId);
+            return View(vacation);
+        }
+
+        // POST: Vacations/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,StartDate,EndDate,CreationDate,IsHalfDay,IsConfirmed,VacationType,SickSheet,UserId")] Vacation vacation)
+        {
+            if (id != vacation.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(vacation);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VacationExists(vacation.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", vacation.UserId);
+            return View(vacation);
+        }
+
+        // GET: Vacations/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vacation = await _context.Vacations
+                .Include(v => v.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vacation == null)
+            {
+                return NotFound();
+            }
+
+            return View(vacation);
+        }
+
+        // POST: Vacations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var vacation = await _context.Vacations.FindAsync(id);
+            if (vacation != null)
+            {
+                _context.Vacations.Remove(vacation);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VacationExists(int id)
+        {
+            return _context.Vacations.Any(e => e.Id == id);
+        }
+    }
+}
